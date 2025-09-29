dff19cfd3c487844b232a86819dc6627f8f8c341
S <siddtheone@gmail.com>
2025-09-29 16:19:09 +0530

    Added help text

--- src/components/MyPortion.tsx
+++ src/components/MyPortion.tsx
@@ -3,6 +3,8 @@
 import { Cookware } from "@/components/Cookware";
 import { useCookware } from "@/store/useCookware";
 import { Notice } from "./Notice";
+import { Alert, Box } from "@mui/material";
+import { Info } from "@mui/icons-material";
 
 export function MyPortion() {
   const cookwares = useCookware((state) => state.cookwares);
@@ -13,6 +15,9 @@ export function MyPortion() {
       {cookwares.map((cookware) => (
         <Cookware key={cookware.id} cookware={cookware} />
       ))}
+      {cookwares.length === 0 && (
+        <Box sx={{ textAlign: "center" }}>Start by adding cookwares</Box>
+      )}
     </>
   );
 }
